af615c8f048a1fc3d37c16ce39b557ba9e372bfe
Pranathi Sidhani <pranathi276sidhani@gmail.com>
2026-06-28 02:15:38 +0530

    Add debug for signup

--- app.js
+++ app.js
@@ -99,10 +99,19 @@ app.all("/*splat", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found"));
 });
 
+// app.use((err, req, res, next) => {
+//   let { statusCode = 500, message = "Something went wrong" } = err;
+//   res.status(statusCode).render("error.ejs", { message });
+//   // res.status(statusCode).send(message);
+// });
+
 app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
   let { statusCode = 500, message = "Something went wrong" } = err;
   res.status(statusCode).render("error.ejs", { message });
-  // res.status(statusCode).send(message);
 });
 
 app.listen(8080, () => {
